9c0a4667ce91880f96131da6022a2bd8a5cc9db1
kilumbumunguma23 <gracekilumbu23@gmail.com>
2022-06-11 10:41:09 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/gestionecole/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gestionecole
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:gestionecole/personne.dart';
+import 'package:gestionecole/update.dart';
+import 'package:gestionecole/deleteP.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as Math;
+
+void main() {
+
+  // runApp(MaterialApp(home: EditPersonne()));
+  runApp(MaterialApp(home: DeletePersonne()));
+  //  runApp(MaterialApp(home: Personne()));
+
+}
